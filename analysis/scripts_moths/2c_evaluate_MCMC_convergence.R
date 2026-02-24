20b0c459a0123a46515653cbdd8ba49277fc4e79
Luisa F. Rodriguez <lufrodriguezca@users.noreply.github.com>
2026-02-24 03:30:41 +0200

    Add files via upload

---
+++ analysis/scripts_moths/2c_evaluate_MCMC_convergence.R
@@ -0,0 +1,35 @@
+library(coda)
+
+for(mname in c("moths")){
+  thin = 5
+  samples = 250
+  nChains = 5
+  load(paste0(paste0("models/fitted_model_", mname, "_samples_", samples, "_thin_", thin, "_nChains_", nChains, ".RData")))
+  mpost = convert_to_coda_object(m)
+#  plot(mpost$Beta)
+#  plot(mpost$Gamma)
+  #pdf(paste0("results/MCMC_convergence_", mname, ".pdf"))
+  plot(mpost$Beta)
+  plot(mpost$Gamma)
+  if(!is.null(m$C)) plot(mpost$rho)
+  summary(gelman.diag(mpost$Beta,multivariate = FALSE)$psrf)
+  summary(gelman.diag(mpost$Gamma,multivariate = FALSE)$psrf)
+  summary(gelman.diag(mpost$rho,multivariate = FALSE)$psrf)
+  #dev.off()
+}
+
+bad.chains = matrix(FALSE, nrow = m$ns, ncol = nChains)
+for(sp in 1:m$ns){
+  li = matrix(ncol = nChains, nrow = samples)
+  for(i in 1:nChains){
+    for(j in 1:samples){
+      li[j, i] = m$postList[[i]][[j]]$li[sp]
+    }
+  }
+  plot(NULL, xlim = c(0, samples), ylim = c(min(li), max(li)), main = sp)
+  for(i in 1:nChains) lines(li[, i])
+  me = colMeans(li)
+  sd = sqrt(diag(var(li)))
+  bad.chains[sp, me < max(me - 2*sd)] = TRUE
+}
+colMeans(bad.chains)
